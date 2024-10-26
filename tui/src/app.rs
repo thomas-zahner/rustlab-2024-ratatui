850a428b30a3853a5c867852950bb8ea8a4bce27
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2024-10-26 22:31:02 +0300

    Initialize TUI with Ratatui

---
+++ tui/src/app.rs
@@ -0,0 +1,35 @@
+use crossterm::event::{Event, EventStream, KeyCode};
+use futures::StreamExt;
+use ratatui::DefaultTerminal;
+
+pub struct App {
+    term_stream: EventStream,
+    is_running: bool,
+}
+
+impl App {
+    pub fn new() -> Self {
+        let term_stream = EventStream::new();
+        Self {
+            term_stream,
+            is_running: false,
+        }
+    }
+
+    pub async fn run(mut self, mut terminal: DefaultTerminal) -> anyhow::Result<()> {
+        self.is_running = true;
+
+        while self.is_running {
+            terminal.draw(|frame| frame.render_widget("Hello Ratatui!", frame.area()))?;
+            if let Some(crossterm_event) = self.term_stream.next().await {
+                let crossterm_event = crossterm_event?;
+                if let Event::Key(key_event) = crossterm_event {
+                    if key_event.code == KeyCode::Esc {
+                        self.is_running = false;
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+}
--- tui/src/main.rs
+++ tui/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+mod app;
+
+use app::App;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let app = App::new();
+    let terminal = ratatui::init();
+    let result = app.run(terminal).await;
+    ratatui::restore();
+    result
 }
