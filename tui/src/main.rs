8d420f253eea3ee6074cec77d104c6751fcf36f8
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2024-10-26 20:28:48 +0300

    Remove TUI initialization

--- tui/src/main.rs
+++ tui/src/main.rs
@@ -1,12 +1,3 @@
-mod app;
-
-use app::App;
-
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let app = App::new();
-    let terminal = ratatui::init();
-    let result = app.run(terminal).await;
-    ratatui::restore();
-    result
+fn main() {
+    println!("Hello, world!");
 }
