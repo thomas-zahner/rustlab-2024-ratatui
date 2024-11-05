76757813cd998b2078e02c2da486d5122cea65cd
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2024-11-05 19:01:58 +0300

    Initialize TUI with Ratatui (solution)

--- tui/src/app.rs
+++ tui/src/app.rs
@@ -17,11 +17,19 @@ impl App {
     }
 
     pub async fn run(mut self, mut terminal: DefaultTerminal) -> anyhow::Result<()> {
-        // Initialize the main event loop:
-        // - Render content to the terminal
-        // - Listen for key events (e.g., Esc key to exit the loop)
-        todo!();
+        self.is_running = true;
 
+        while self.is_running {
+            terminal.draw(|frame| frame.render_widget("Hello Ratatui!", frame.area()))?;
+            if let Some(crossterm_event) = self.term_stream.next().await {
+                let crossterm_event = crossterm_event?;
+                if let Event::Key(key_event) = crossterm_event {
+                    if key_event.code == KeyCode::Esc {
+                        self.is_running = false;
+                    }
+                }
+            }
+        }
         Ok(())
     }
 }
